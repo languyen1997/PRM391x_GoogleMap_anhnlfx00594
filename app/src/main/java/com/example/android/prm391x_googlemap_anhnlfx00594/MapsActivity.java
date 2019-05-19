6c7fd07283142ed6fe346ee6c0d2c6c2c52819e6
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2019-05-19 18:43:07 +0700

    Currently Working. Fix Direction API not working. Delete unnecessary MainActivity.

--- app/src/main/java/com/example/android/prm391x_googlemap_anhnlfx00594/MapsActivity.java
+++ app/src/main/java/com/example/android/prm391x_googlemap_anhnlfx00594/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.android.prm391x_googlemap_anhnlfx00594;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -17,8 +16,10 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -47,15 +48,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     GoogleMap mMap;
     private boolean isLocationPermissionGranted = false;
     private GeoApiContext geoApiContext = null;
-    private LatLng userPosition;
 
     private static final float DEFAULT_ZOOM = 15f;
     private static final String PERMISSION_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 3232;
-    private static final int DEFAULT_PADDING = -1;
-
-    public static final String PATH_DISTANCE = "pathDistance";
-    public static final String PATH_DURATION = "pathDuration";
+    private static final int DEFAULT_PADDING = 150;
 
     private static final float MARKER_COLOR_BLUE = BitmapDescriptorFactory.HUE_RED;
     private static final float MARKER_COLOR_RED = BitmapDescriptorFactory.HUE_BLUE;
@@ -69,7 +66,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (isLocationPermissionGranted) {
             // Obtain the SupportMapFragment and get notified when the map is ready to be used.
             SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-            supportMapFragment.getMapAsync(this);
+            if (supportMapFragment != null) {
+                supportMapFragment.getMapAsync(this);
+            }
         }
 
         Button button = findViewById(R.id.find_path_button);
@@ -107,12 +106,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getCurrentPosition();
     }
 
+    /**
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
     private void getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
+
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -150,34 +150,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener((@NonNull Task<Location> task) -> {
             Location currentLocation = task.getResult();
             if (task.isSuccessful() && currentLocation != null) {
-                userPosition = toLatLng(currentLocation);
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toLatLng(currentLocation), DEFAULT_ZOOM));
                 mMap.setMyLocationEnabled(true);
             } else {
-                Toast.makeText(MapsActivity.this, "Unable to find your location", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MapsActivity.this, R.string.current_location_failed, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void geoLocate(String originAddress, String destinationAddress) {
         Geocoder geocoder = new Geocoder(this);
-        LatLng startPoint = userPosition;
-        LatLng endPoint;
 
-        if (!originAddress.isEmpty()) {
-            List<Address> originAddressList = new ArrayList<>();
-            try {
-                originAddressList = geocoder.getFromLocationName(originAddress, 1);
-            } catch (IOException e) {
-                Log.e("MyDebug", "geoLocate originAddress", e);
-            }
-
-            if (originAddressList.size() > 0) {
-                startPoint = toLatLng(originAddressList.get(0));
-
-                placeMarker(startPoint, "Origin Address", MARKER_COLOR_RED);
-            }
+        List<Address> originAddressList = new ArrayList<>();
+        try {
+            originAddressList = geocoder.getFromLocationName(originAddress, 1);
+        } catch (IOException e) {
+            Log.e("MyDebug", "geoLocate originAddress", e);
         }
+
+        LatLng startPoint = toLatLng(originAddressList.get(0));
+        placeMarker(startPoint, getString(R.string.origin_marker), MARKER_COLOR_RED);
 
         List<Address> destinationAddressList = new ArrayList<>();
         try {
@@ -186,13 +178,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.e("MyDebug", "geoLocate destinationAddress", e);
         }
 
-        if (destinationAddressList.size() > 0) {
-            endPoint = toLatLng(destinationAddressList.get(0));
+        LatLng endPoint = toLatLng(destinationAddressList.get(0));
 
-            placeMarker(endPoint, "Destination Address", MARKER_COLOR_BLUE);
-            calculateDirection(startPoint, endPoint);
-            moveCameraToPath(startPoint, endPoint);
-        }
+        placeMarker(endPoint, getString(R.string.destination_marker), MARKER_COLOR_BLUE);
+        calculateDirection(startPoint, endPoint);
     }
 
     private void placeMarker(LatLng latLng, String title, float color) {
@@ -211,13 +200,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         directionsApiRequest.setCallback(new com.google.maps.PendingResult.Callback<DirectionsResult>() {
             @Override
             public void onResult(DirectionsResult result) {
-                Log.v("MyDebug", "Distance: " + result.routes[0].legs[0].distance.toString());
-                Log.v("MyDebug", "Duration: " + result.routes[0].legs[0].duration.toString());
-
-                Intent intent = new Intent(MapsActivity.this, MainActivity.class);
-                intent.putExtra(PATH_DISTANCE, result.routes[0].legs[0].distance.toString());
-                intent.putExtra(PATH_DURATION, result.routes[0].legs[0].duration.toString());
-                startActivity(intent);
+                TextView distanceTextView = findViewById(R.id.path_distance);
+                TextView durationTextView = findViewById(R.id.path_duration);
 
                 DirectionsRoute[] directionsRoute = result.routes;
                 List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(directionsRoute[0].overviewPolyline.getEncodedPath());
@@ -228,6 +212,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
 
                 runOnUiThread(() -> {
+                    distanceTextView.setText(result.routes[0].legs[0].distance.toString());
+                    durationTextView.setText(result.routes[0].legs[0].duration.toString());
+                    moveCameraToPath(result.routes[0].bounds.southwest, result.routes[0].bounds.northeast);
                     Polyline polyline = mMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
                     polyline.setColor(R.color.colorAccent);
                 });
@@ -240,47 +227,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    /*private void drawPath(DirectionsResult result) {
-        new Handler(Looper.getMainLooper()).post(() -> {
-            List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(result.routes[0].overviewPolyline.getEncodedPath());
-            List<LatLng> newDecodedPath = new ArrayList<>();
+    private void moveCameraToPath(com.google.maps.model.LatLng startPoint, com.google.maps.model.LatLng endPoint) {
 
-            for (com.google.maps.model.LatLng latLng : decodedPath) {
-                newDecodedPath.add(toLatLng(latLng));
-            }
-            Polyline polyline = mMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
-            polyline.setColor(R.color.colorAccent);
-        });
-    }*/
-
-    private void moveCameraToPath(LatLng startPoint, LatLng endPoint) {
-        double topBound;
-        double bottomBound;
-        double leftBound;
-        double rightBound;
-
-        if (startPoint.latitude > endPoint.latitude) {
-            topBound = startPoint.latitude;
-            bottomBound = endPoint.latitude;
-        } else {
-            topBound = endPoint.latitude;
-            bottomBound = startPoint.latitude;
-        }
-
-        if (startPoint.longitude > endPoint.longitude) {
-            rightBound = startPoint.longitude;
-            leftBound = endPoint.longitude;
-        } else {
-            rightBound = endPoint.longitude;
-            leftBound = startPoint.longitude;
-        }
-
-        LatLngBounds latLngBounds = new LatLngBounds(toLatLng(bottomBound, leftBound), toLatLng(topBound, rightBound));
+        LatLngBounds latLngBounds = new LatLngBounds(toLatLng(startPoint), toLatLng(endPoint));
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, DEFAULT_PADDING));
-    }
-
-    private LatLng toLatLng(double latitude, double longitude) {
-        return new LatLng(latitude, longitude);
     }
 
     private LatLng toLatLng(Location location) {
@@ -291,7 +241,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return new LatLng(address.getLatitude(), address.getLongitude());
     }
 
-    private static LatLng toLatLng(com.google.maps.model.LatLng latLng) {
+    private LatLng toLatLng(com.google.maps.model.LatLng latLng) {
         return new LatLng(latLng.lat, latLng.lng);
     }
 
